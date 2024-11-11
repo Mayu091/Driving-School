5904610ebfb4a972a440537bba4aa827d0f41eda
Mayu091 <mayurithipe1101@gmail.com>
2024-11-11 13:47:21 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Montserrat':['Montserrat', 'sans-serif'],
+        'Courgette': ['Courgette', 'cursive'],
+        'Oswald':['Oswald', 'sans-serif'] 
+      }
+    },
+  },
+  plugins: [],
+}
+
